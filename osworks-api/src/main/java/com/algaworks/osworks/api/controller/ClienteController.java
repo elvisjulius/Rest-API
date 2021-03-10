4266b7682dd2bebe8a878d8d61288f29add8af58
Elvis Julius <elvisjulius@hotmail.com>
2021-03-10 18:04:46 -0300

    Rest API + SQL

--- osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ osworks-api/src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -4,27 +4,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Elvis Julius");
-		cliente1.setEmail("elvisjulius@hotmail.com");
-		cliente1.setTelefone("974449609");
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Evelin");
-		cliente2.setEmail("evelin@hotmail.com");
-		cliente2.setTelefone("3222215469");
-		return Arrays.asList (cliente1, cliente2);
+		return clienteRepository.findAll();
+
 	}
 
 }
