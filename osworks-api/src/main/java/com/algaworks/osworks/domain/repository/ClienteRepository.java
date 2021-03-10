16419bdbb53664aa1c2ac61b28923aebd5d31a80
Elvis Julius <elvisjulius@hotmail.com>
2021-03-10 18:04:58 -0300

    .

--- osworks-api/src/main/java/com/algaworks/osworks/domain/repository/ClienteRepository.java
+++ osworks-api/src/main/java/com/algaworks/osworks/domain/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.algaworks.osworks.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import com.algaworks.osworks.domain.model.Cliente;
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 	 	
 
+	List<Cliente> findByNome(String nome);
 }
